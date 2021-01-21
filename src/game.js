ba10b9d001e53fece97e3ee36b4b46893951e0ed
elsstrange <ellenabstrange@gmail.com>
2021-01-21 20:56:49 +0000

    User can move gamepiece with arrow keys Gamepiece does not exit the canvas on either side.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 function startGame() {
   myGameArea.start();
+  myGamePiece = new component(30, 30, "red", (myGameArea.canvas.width/2) - 15, myGameArea.canvas.height - 30);
 }
 
 let myGameArea = {
@@ -10,9 +11,39 @@ let myGameArea = {
     this.canvas.style = "border: 2px solid"
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+}
+
+let myGamePiece;
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function(){
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.update();
 }
 
 window.addEventListener('load', () => {
   startGame();
+  window.addEventListener('keydown', (event) => {
+    if (event.keyCode === 39 && myGamePiece.x < myGameArea.canvas.width - myGamePiece.width) {
+      myGamePiece.x += 5;
+    } else if (event.keyCode === 37 && myGamePiece.x > 0) {
+      myGamePiece.x -= 5;
+    };
+  });
 });
