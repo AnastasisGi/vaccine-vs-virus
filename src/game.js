5cdcb8e8812083ba0db09d57a94578f763643069
AnastasisGi <anastasiosgks@gmail.com>
2021-01-21 20:36:38 +0000

    Static block created

--- src/game.js
+++ src/game.js
@@ -1,4 +1,7 @@
+var obstacleBlock;
+
 function startGame() {
+  obstacleBlock = new component (50, 50, 'green', 300, 120);
   myGameArea.start();
 }
 
@@ -10,7 +13,36 @@ let myGameArea = {
     this.canvas.style = "border: 2px solid"
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+}
+
+// function for the obstacles
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+// function updateGame
+function updateGameArea() {
+  myGameArea.clear();
+  obstacleBlock.update();
 }
 
 window.addEventListener('load', () => {
