df3f096a6d99fdf77a59ec5182cfcf6887ea416f
Amy <amee_sk@yahoo.com>
2021-01-28 18:57:25 +0000

    Created test for clear function

--- spec/gameArea.test.js
+++ spec/gameArea.test.js
@@ -9,28 +9,39 @@ let componentWidth = 10;
 let componentHeight = 20;
 let componentX = 30;
 let componentY = 40;
+let startX = 0;
+let startY = 0;
 
-beforeEach(()=> {
+beforeEach(() => {
   mockContext = {
-    fillRect: () => {},
+    fillRect: () => { },
+    clearRect: () => { }
   }
 
   mockCanvas = {
-    getContext: ()=> { return mockContext }
+    getContext: () => { return mockContext }
   }
 
   testGameArea = new gameArea(width, height, mockCanvas)
 })
 
-describe('rendering a rectangular object', ()=> {
-  test('it fills the object with the specified colour', ()=> {
+describe('rendering a rectangular object', () => {
+  test('it fills the object with the specified colour', () => {
     testGameArea.drawRectangularObject(0, 0, 'red', 0, 0);
     expect(mockContext.fillStyle).toEqual('red');
   })
 
-  test('it draws a rectangle with the specified coordinates', ()=>{
+  test('it draws a rectangle with the specified coordinates', () => {
     jest.spyOn(mockContext, 'fillRect');
     testGameArea.drawRectangularObject(componentWidth, componentHeight, 'red', componentX, componentY);
     expect(mockContext.fillRect).toHaveBeenCalledWith(componentX, componentY, componentWidth, componentHeight);
   })
-});
+})
+
+describe('deleting a rectangular object', () => {
+  test('it deletes the object', () => {
+    jest.spyOn(mockContext, 'clearRect');
+    testGameArea.clearRectangularObject(startX, startY)
+    expect(mockContext.clearRect).toHaveBeenCalledWith(startX, startY, testGameArea.canvas.width, testGameArea.canvas.height)
+  })
+})
--- src/gameArea.js
+++ src/gameArea.js
@@ -1,11 +1,17 @@
 function gameArea(width, height, canvas) {
   this.canvas = canvas;
+  this.canvas.width = width;
+  this.canvas.height = height;
   this.context = this.canvas.getContext('2d');
 
   this.drawRectangularObject = (width, height, colour, x, y) => {
     this.context.fillStyle = colour;
     this.context.fillRect(x, y, width, height);
   }
+
+  this.clearRectangularObject = (x, y) => {
+    this.context.clearRect(x, y, this.canvas.width, this.canvas.height);
+  }
 }
 
-export {gameArea};
+export { gameArea };
