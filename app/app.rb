1e66600fda3e9f18a7afc55d4ac44f5ff5ce5fb3
elsstrange <ellenabstrange@gmail.com>
2021-02-07 15:02:27 +0000

    Set up continuous deployment workflow Uses github actions, runs tests and deploys to Heroku on push to main branch Added a /health route to use for testing successful deployment and automating rollback in the case of failure.

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,11 @@ require 'sinatra/base'
 
 class VaccineVsVirus < Sinatra::Base
   get '/' do
-    # send_file File.join(settings.public_folder, 'otherindex.html')
     send_file 'app/views/index.html'
+  end
+
+  # This route exists to test successful deployment to Heroku via Github Actions
+  get '/health' do
+    "ok"
   end
 end
