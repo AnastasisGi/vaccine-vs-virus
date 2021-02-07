a31cfb29b75407fe9c252903d3352c589afb365d
elsstrange <ellenabstrange@gmail.com>
2021-02-07 12:00:01 +0000

    Increases score and renders it into element

--- src/score.js
+++ src/score.js
@@ -1,21 +1,14 @@
 function score (element) {
 
-  this.element = element
-  this.element.innerHTML = '<p id="score">0</p>'
+  this.element = element;
+  this.score = 0;
+    
+  this.element.innerHTML = `<p id="score">${this.score}</p>`;
 
-
-
+  this.increaseScore = (addition) => {
+    this.score += addition;
+    this.element.innerHTML = `<p id="score">${this.score}</p>`;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 export { score };
